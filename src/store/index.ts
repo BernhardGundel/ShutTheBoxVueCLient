af7e60455ccb46b69b80f7afe67307bdda58d0ff
LugsoIn2 <timmy.pfau@lugso.de>
2021-01-17 00:11:29 +0100

    change secure conf

--- src/store/index.ts
+++ src/store/index.ts
@@ -56,7 +56,7 @@ const store = new Vuex.Store({
   },
   actions: {
     login({ commit }, user) {
-      axios.post("http://" + server + "/signIn", user, $.extend(axiosConfig, {
+      axios.post("https://" + server + "/signIn", user, $.extend(axiosConfig, {
         headers: {
           'Content-Type': 'application/x-www-form-urlencoded'
         }
@@ -71,7 +71,7 @@ const store = new Vuex.Store({
     },
 
     logout({ commit }) {
-      axios.get("http://" + server + "/signOut", axiosConfig)
+      axios.get("https://" + server + "/signOut", axiosConfig)
         .then(() => {
           router.push("/login");
           commit('SET_COOKIE', document.cookie)
@@ -82,7 +82,7 @@ const store = new Vuex.Store({
     },
 
     register({ commit }, user) {
-      axios.post("http://" + server + "/signUp", user, $.extend(axiosConfig, {
+      axios.post("https://" + server + "/signUp", user, $.extend(axiosConfig, {
         headers: {
           'Content-Type': 'application/x-www-form-urlencoded'
         }
