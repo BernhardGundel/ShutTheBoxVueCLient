9d1ccc3d68dc9bf636b2186010cd1f57afd76fa8
BernhardGundel <bernhard.gundel@htwg-konstanz.de>
2021-01-17 18:10:59 +0100

    blub

--- src/store/index.ts
+++ src/store/index.ts
@@ -7,7 +7,7 @@ import axios from "axios";
 Vue.use(Vuex);
 
 const server = "shuttheboxserver.herokuapp.com"
-const websocket = new WebSocket("wss://" + "3.225.119.137" + "/websocket");
+const websocket = new WebSocket("wss://" + server + "/websocket");
 const axiosConfig = {
   withCredentials: true,
   headers: {
